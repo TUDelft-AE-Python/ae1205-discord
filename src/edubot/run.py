f662b40a3f9a46d3f318340fd9ce42446955ce66
skilkis <sankilkis@msn.com>
2020-04-27 17:40:30 +0200

    Add capability to run EduBot from IPython
    This commit allows running EduBot from an interactive console (IPython)
    and thus make it easier to see variables in real-time while debugging.
    Note this does not mean that one has to have IPython installed, which
    is why IPython is not added to setup.cfg.
    
    Summary of Changes:
    - Revert to using package import for EduBot (this has proven to be the
      most stable for all forms of running EduBot)
    - Add BotRunner and InteractiveBotRunner classes to support launching
      EduBot from IPython

--- src/edubot/run.py
+++ src/edubot/run.py
@@ -15,14 +15,15 @@
 # License along with this program.
 # If not, see <https://www.gnu.org/licenses/>.
 
-"""Contains the :py:func:`cli` to run EduBot from command-line."""
+"""Contains functions and classes for running :py:class:`EduBot`."""
 
-
+import asyncio
 import os
+from typing import Optional
 
 import click
 
-from .bot import EduBot
+from edubot.bot import EduBot
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
@@ -36,11 +37,83 @@ def cli(token: str):
         token: Discord API Token
 
     """
-    assert token is not None, "No Discord API token could be retrieved"
-    bot = EduBot()
-    bot.run(token)
-    return bot
+    return BotRunner(token)
+
+
+class BotRunner:
+    """Runs :py:class:`EduBot` in the standard blocking manner."""
+
+    def __init__(self, token: Optional[str] = TOKEN):
+        self.validate_token(token)
+        self.bot = EduBot()
+        self.run(token)
+
+    def run(self, token):
+        """Runs the bot with the given ``token`` as a blocking call."""
+        self.bot.run(token)
+
+    # TODO find a better validation method later or remove completely
+    @staticmethod
+    def validate_token(token: str) -> None:
+        """Ensures that the provided ``token`` is valid."""
+        _error_msg = "No Discord API token could be retrieved"
+        assert isinstance(token, str) and len(token) != 0, _error_msg
+
+
+class InteractiveBotRunner(BotRunner):
+    """Runs :py:class:`EduBot` in a non-blocking manner.
+
+    This is suitable for running the bot with interactive consoles such
+    as IPython.
+
+    Caution:
+        No clean-up action will be called after the Interactive Console
+        is closed therefore no saving operations will take place. DO NOT
+        USE THIS FOR ACTUAL SESSIONS!
+    """
+
+    def __init__(self, token: Optional[str] = TOKEN):
+        self.loop = asyncio.get_event_loop()
+        self.loop.set_debug(True)
+        super().__init__(token)
+
+    def run(self, token):
+        """Retrieves the active event loop and runs the bot on it.
+
+        By retrieving the event-loop (if it exists) with
+        :py:func:`asyncio.get_event_loop`, we guarantee that the
+        :py:class:`EduBot` will integrate into the already present
+        event-loop of the interactive console (IPython).
+        """
+        self.create_task(self.bot.start(token))
+
+    def create_task(self, task):
+        """Runs a given ``task`` asynchroniously using asyncio.
+
+        The purpose of this function is to simply reduce the amount of
+        code that needs to be typed when a command needs to be issued
+        from within the interactive console. All this method does is
+        call :py:func:`asyncio.loop.create_task` on the given ``task``.
+        """
+        self.loop.create_task(task)
+
+
+def is_ipython() -> bool:
+    """Determines if IPython is currently running.
+
+    Note:
+        Encapsulating the `get_ipython` call with this function avoids
+        having to force developers/users to install IPython for the rest
+        of the functionality to work.
+    """
+    try:
+        from IPython import get_ipython
+
+        return get_ipython()
+    except ImportError:
+        return False
 
 
 if __name__ == "__main__":
-    bot = cli()  # Assignment of the bot prevents gc (garbage-collection)
+    runner = InteractiveBotRunner(TOKEN) if is_ipython() else BotRunner(TOKEN)
+    bot = runner.bot
