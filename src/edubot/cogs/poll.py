8de9e6631653bb5635d2e410fd48f1d30da77426
CryLyo <37627822+CryLyo@users.noreply.github.com>
2020-05-06 19:11:24 +0200

    expanded emoji list to 36 answer options

--- src/edubot/cogs/poll.py
+++ src/edubot/cogs/poll.py
@@ -35,7 +35,16 @@ class Quiz:
         self.votes = {}
 
         self.emoji_options = [get_emoji(em) for em in
-                              (":one:",":two:",":three:",":four:",":five:",":six:",":seven:",":eight:",":nine:")]
+                              (":one:",":two:",":three:",":four:",":five:",":six:",":seven:",":eight:",":nine:",
+                               ":keycap_ten:",":regional_indicator_a:", ":regional_indicator_b:",
+                               ":regional_indicator_c:", ":regional_indicator_d:", ":regional_indicator_e:",
+                               ":regional_indicator_f:", ":regional_indicator_g:", ":regional_indicator_h:",
+                               ":regional_indicator_i:", ":regional_indicator_j:", ":regional_indicator_k:",
+                               ":regional_indicator_l:", ":regional_indicator_m:", ":regional_indicator_n:",
+                               ":regional_indicator_o:", ":regional_indicator_p:", ":regional_indicator_q:",
+                               ":regional_indicator_r:", ":regional_indicator_s:", ":regional_indicator_t:",
+                               ":regional_indicator_u:", ":regional_indicator_v:", ":regional_indicator_w:",
+                               ":regional_indicator_x:", ":regional_indicator_y:", ":regional_indicator_z:")]
 
     def load_data(self):
         '''Function for loading in json files containing the quiz information'''
